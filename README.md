c75df2dfc5851261c8888f8410977f9ceb3e9f17
Jigish Solanki <jigishsolanki111@gmail.com>
2023-04-26 08:41:50 +0530

    Books - CRUD

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ReactJs CRUD Books
 ReactJs CRUD for book module
+
+https://reactjs-crud-books.vercel.app/
--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import "./index.css";
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    setBooks([...books, { id: Math.round(Math.random() * 9999), title }]);
+  };
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const editBookById = (id, title) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title };
+      }
+      return book;
+    });
+
+    setBooks(updatedBooks);
+  };
+
   return (
-    <div>
-      Hello World
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
+      <BookCreate onCreate={createBook} />
     </div>
   );
 }
